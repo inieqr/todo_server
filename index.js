5b2b24358ddcc56e997b32fb89aea9f1b1997254
Iniobong Equere <inieqr@gmail.com>
2024-03-23 01:41:09 +0200

    Server

---
+++ index.js
@@ -0,0 +1,16 @@
+process.setMaxListeners(15); // Set the number according to your needs
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { todoRouter } = require('./routes/todo.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', todoRouter);
+
+const port = process.env.PORT;
+
+app.listen(port);
